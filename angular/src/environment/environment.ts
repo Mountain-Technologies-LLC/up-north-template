170cd08e6755b08c19348ac27ea828f070d325fe
Anthony Pearson <ajgoldenwings@gmail.com>
2024-12-10 19:35:08 -0600

    Update environment.ts

--- angular/src/environment/environment.ts
+++ angular/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  demoSchemaUrl: 'https://raw.githubusercontent.com/Mountain-Technologies-LLC/up-north-template/refs/heads/main/template-app/src/schema/demo.json',
+  demoSchemaUrl: 'https://raw.githubusercontent.com/Mountain-Technologies-LLC/up-north-template/refs/heads/main/angular/src/schema/demo.json',
   blankSchemaUrl: '/blankSchema.json',
   schemaUrl: '',
 };
