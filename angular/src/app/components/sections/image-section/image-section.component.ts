088ef5d3f834ca76347e51c5a50568b4a78438aa
Anthony Pearson <ajgoldenwings@gmail.com>
2025-01-20 09:15:59 -0600

    Remove image classes
    Angular was not rendering classes properly

--- angular/src/app/components/sections/image-section/image-section.component.ts
+++ angular/src/app/components/sections/image-section/image-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { ImageSection } from '../../../../schema';
 import { APP_BASE_HREF, NgClass,  NgFor,  NgIf } from '@angular/common';
 
@@ -7,20 +7,9 @@ import { APP_BASE_HREF, NgClass,  NgFor,  NgIf } from '@angular/common';
   imports: [NgClass, NgFor, NgIf],
   templateUrl: './image-section.component.html',
 })
-export class ImageSectionComponent implements OnInit {
+export class ImageSectionComponent {
   constructor (@Inject(APP_BASE_HREF) public baseHref: string) { }
-
-  ngOnInit(): void {
-    if (this.imageSection.imageFileNames != null) {
-      let length = this.imageSection.imageFileNames.length;
-      this.imageListClasses.push("grid-cols-" + Math.min(length, 2));
-      this.imageListClasses.push("sm:grid-cols-" + Math.min(length, 3));
-      this.imageListClasses.push("md:grid-cols-" + Math.min(length, 4));
-    }
-  }
 
   @Input()
   imageSection!: ImageSection;
-
-  imageListClasses: string[] = [];
 }
