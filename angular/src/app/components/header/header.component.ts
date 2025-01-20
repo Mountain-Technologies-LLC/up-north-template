11590fd930564177ab2eb571d5eaf06ea608ac62
Anthony Pearson <ajgoldenwings@gmail.com>
2025-01-20 12:42:46 -0600

    Show/Hide shadow

--- angular/src/app/components/header/header.component.ts
+++ angular/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, HostListener, input, Renderer2 } from '@angular/core';
 import { Schema } from '../../../schema';
 import { NgClass, NgIf } from '@angular/common';
 import { NavbarComponent } from "./navbar/navbar.component";
@@ -13,6 +13,24 @@ import { EditMenuComponent } from './edit-menu/edit-menu.component';
 export class HeaderComponent {
   editing = input<boolean>();
   schema = input<Schema>();
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if (event.target instanceof Document
+      && event.target.scrollingElement instanceof Element
+      && event.target.scrollingElement.scrollTop > 0)
+    {
+      this.renderer.addClass(this.el.nativeElement, 'shadow-md');
+      this.renderer.addClass(this.el.nativeElement, 'shadow-neutral/5');
+    }
+    else
+    {
+      this.renderer.removeClass(this.el.nativeElement, 'shadow-md');
+      this.renderer.removeClass(this.el.nativeElement, 'shadow-neutral/5');
+    }
+  }
 
   showEditMenu = false;
   hideEditMenu() {
